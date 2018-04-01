ccd046d598ac45a7bc53ca4fae184704c60892c8
Alex Santos <hello@alexcsantos.com>
2018-04-02 00:58:16 +0100

    Move sidekiq module into its own directory

--- lib/atomic_sidekiq.rb
+++ lib/atomic_sidekiq.rb
@@ -1,5 +1,5 @@
 require "sidekiq"
-require_relative "atomic_sidekiq/sidekiq"
+require_relative "sidekiq/sidekiq"
 require_relative "atomic_sidekiq/unit_of_work"
 require_relative "atomic_sidekiq/atomic_fetch"
 require_relative "atomic_sidekiq/dead_job_collector"
